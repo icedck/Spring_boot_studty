850c23855ca783503eae408ba7ab3ebae54f02bd
Phạm Quốc Khánh <160307836+icedck@users.noreply.github.com>
2025-06-23 11:10:09 +0700

    Revert "issue JWT token"

--- identity-service/src/main/java/com/example/identity_service/controller/AuthenticationController.java
+++ identity-service/src/main/java/com/example/identity_service/controller/AuthenticationController.java
@@ -2,11 +2,8 @@ package com.example.identity_service.controller;
 
 import com.example.identity_service.dto.request.ApiResponse;
 import com.example.identity_service.dto.request.AuthenticationRequest;
-import com.example.identity_service.dto.request.IntrospectRequest;
 import com.example.identity_service.dto.response.AuthenticationResponse;
-import com.example.identity_service.dto.response.IntrospectReponse;
 import com.example.identity_service.service.AuthenticationService;
-import com.nimbusds.jose.JOSEException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -15,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.ParseException;
-
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -24,19 +19,11 @@ import java.text.ParseException;
 public class AuthenticationController {
     AuthenticationService authenticationService;
 
-    @PostMapping("/token")
+    @PostMapping("/log-in")
     ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
-        var result = authenticationService.authenticate(authenticationRequest);
+        boolean reault = authenticationService.authenticate(authenticationRequest);
         return ApiResponse.<AuthenticationResponse>builder()
-                .result(result)
-                .build();
-    }
-
-    @PostMapping("/introspect")
-    ApiResponse<IntrospectReponse> authenticate(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
-        var result = authenticationService.introspect(request);
-        return ApiResponse.<IntrospectReponse>builder()
-                .result(result)
+                .result(AuthenticationResponse.builder().authenticated(reault).build())
                 .build();
     }
 }
--- identity-service/src/main/java/com/example/identity_service/mapper/UserMapper.java
+++ identity-service/src/main/java/com/example/identity_service/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import com.example.identity_service.dto.request.UserUpdateRequest;
 import com.example.identity_service.dto.response.UserResponse;
 import com.example.identity_service.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
